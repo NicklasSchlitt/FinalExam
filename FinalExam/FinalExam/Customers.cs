347866956099f61fe4c5a9be47fa5df0977f2d29
NicklasSchlitt <43045222+NicklasSchlitt@users.noreply.github.com>
2019-03-18 13:14:52 -0400

    Take this heckboi

--- FinalExam/FinalExam/Customers.cs
+++ FinalExam/FinalExam/Customers.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace FinalExam
 {
@@ -27,6 +28,39 @@ namespace FinalExam
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void AddNmeBtn_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Add(textBox1, textBox4, textBox3, textBox2);
+        }
+
+        private void RemveNmeBtn_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow item in this.dataGridView1.SelectedRows)
+            {
+                dataGridView1.Rows.RemoveAt(item.Index);
+            }
+        }
+
+        private void BindGrid()
+        {
+            string constring = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=S:\repos\FinalExam\FinalExam\FinalExam\Customersdatabase.mdf;Integrated Security=True";
+            using (SqlConnection con = new SqlConnection(constring))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM Customers", con))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        using (DataTable dt = new DataTable())
+                        {
+                            sda.Fill(dt);
+                            dataGridView1.DataSource = dt;
+                        }
+                    }
+                }
+            }
         }
     }
 }
